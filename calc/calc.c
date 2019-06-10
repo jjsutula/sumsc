aae77f418d5c7f686dd5f635ab7897e3b44e695f
jjsutula <jsutula@workpoint.com>
2019-06-09 22:29:34 -0600

    code calc inputs

--- calc/calc.c
+++ calc/calc.c
@@ -1,7 +1,58 @@
 #include <stdio.h>
+#include <calc.h>
+
+void calculator() {
+    char quit = ' ';
+    struct in_s in = {.left=0, .right=0, .op=' '};
+
+    printf("Anyone?\n");
+    
+    
+    while (quit != 'q') {
+        gatherInput(&in);
+        if (in.op == 'q') {
+            quit = 'q';
+        } else {
+            printf("Left: %d, Right: %d, Op: '%c'\n", in.left, in.right, in.op);
+        }
+    }
+    
+    /*
+            res = doMath(left, parts[1], right)
+            print(str(left) + ' ' + parts[1] + ' ' + str(right) + ' = ' + str(res))
+     */
+}
+
+void gatherInput(struct in_s *in ) {
+    int left;
+    int right;
+    char op;
+    int retval;
+
+    printf("Enter an equation: ");
+    retval = scanf("%d %c %d", &left, &op, &right);
+//    char *c = entered;
+//    for (char *c = entered; *c != '\0'; c++) {
+//        printf("%c\n", *c);
+//    }
+    if (retval == 0) {
+        in->op = 'q';
+    } else {
+        in->left = left;
+        in->right = right;
+        in->op = op;
+    }  
+}
+
+int doMath(struct in_s *in) {
+    return 10;
+}
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
-	return 0;
+    calculator();
+    printf("We done.\n");
+    char entered[100];
+    scanf("%s", entered);
+    return 0;
 }
